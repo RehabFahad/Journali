36967f42a16719470d91fc74d567c7051d015175
rehab <1rehabalizzi@gmail.com>
2025-10-19 17:01:25 +0300

    Initial Commit

---
+++ Journali/JournaliApp.swift
@@ -0,0 +1,17 @@
+//
+//  JournaliApp.swift
+//  Journali
+//
+//  Created by رحاب فهد  on 27/04/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct JournaliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
